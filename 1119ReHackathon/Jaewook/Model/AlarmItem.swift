1176547fb7e5b1a7c603be7baeccf09c5dc645b5
e-sung <dev.esung@gmail.com>
2017-11-19 21:00:10 +0900

    Add Comments on AlarmItem.swift

--- 1119ReHackathon/Jaewook/Model/AlarmItem.swift
+++ 1119ReHackathon/Jaewook/Model/AlarmItem.swift
@@ -10,23 +10,21 @@ import Foundation
 
 struct AlarmItem: Codable {
     
-    /// 다음 알람시간까지의 초
+    /// 일어날 시간 (단위 : 초 / 기준 : 0시0분0초)
     var timeToWakeUp: TimeInterval = 0
-    /// 전기장판 킬 시간(단위 : 초)
+    /// 전기장판 킬 시간(단위 : 초 / 기준 : 일어날 시간)
     var timeToHeat:TimeInterval = 30.0*60.0
     /// 활성화 여부 : 메인화면에서, 스위치를 토글함으로써 변경.
+    /// - ToDo : 현시점에서 그냥 무늬만 있는 상태. 세부내용 구현 필요
     var isActive:Bool = true
-    /// 스누즈 할 양(단위: 초)
+    /// 스누즈 할 시간
     var snoozeAmount:TimeInterval = 15.0*60.0
     /// 이 알람이 울려야 할 날들 : [월,화,수,목,금,토,일] 중 복수선택
     var repeatDays:[Day] = [.Mon,.Tue,.Wed,.Thu,.Fri]
-    
+    /// repeatDays를 문자열로 변환하는 연산속성
     var repeatDaysString: String {
-        
         var daysString: String = ""
-        
         for item in repeatDays{
-            
             switch item{
             case .Sun:
                 daysString += "일 "
@@ -42,18 +40,18 @@ struct AlarmItem: Codable {
                 daysString += "금 "
             case .Sat:
                 daysString += "토 "
-                
-            
-            
             }
-            
-            
         }
-        
         return daysString
-    
     }
     
+    /**
+     주어진 alarms 배열을 필터링하여 , 특정 day에 울리는 알람들의 배열을 반환
+     
+     - parameter day : 필터 기준
+     - parameter alarms : 필터 대상
+     - parameter shouldBeSorted : true로 설정하면, "가장 먼저 울릴 순"으로 정렬된 배열이 나옴
+     */
     static func availableAlarms(on day:Day, given alarms:[AlarmItem], sorted:Bool = true)->[AlarmItem]{
         var alarmsToReturn:[AlarmItem] = []
         for item in alarms{
--- 1119ReHackathon/Jaewook/Model/Day.swift
+++ 1119ReHackathon/Jaewook/Model/Day.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 
+/**
+ 월화수목금토일
+ - Note :
+ DateComponents(.day)도 Sunday를 '1'로 잡음
+ 특별한 이유가 있지 않으면, 여기에 설정된 rawValue를 바꾸지 말 것
+ */
 enum Day: Int, Codable{
-    
     case Sun = 1
     case Mon = 2
     case Tue = 3
@@ -17,5 +22,4 @@ enum Day: Int, Codable{
     case Thu = 5
     case Fri = 6
     case Sat = 7
-    
 }
